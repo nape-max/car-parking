542b17f2142bb190969d9a46a27b690e5f8fae32
napemax <napemax@yandex.ru>
2020-12-16 12:16:53 +0400

    Initialize empty project.

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=car_parking',
+    'username' => 'car_parking',
+    'password' => 'parking!',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
